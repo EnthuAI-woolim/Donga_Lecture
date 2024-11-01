d7bbfbeba87d9aed41640136bee81415a1c9fdb5
EnthuAI-woolim <markyu428@gmail.com>
2024-11-02 02:22:22 +0900

    2024-11-02 02:22:22

--- Donga_24-2/FilePrecessing/Assignments/a1/replacement_2254747.py
+++ Donga_24-2/FilePrecessing/Assignments/a1/replacement_2254747.py
@@ -1,100 +1,92 @@
-def read_input_file(filename):
+def read_file(filename):
     with open(filename, 'r') as file:
-        datas = []
-        # 첫 줄에서 테스트 케이스 수를 읽어옴
+
         n_cases = int(file.readline().strip())
 
-        # 각 테스트 케이스에 대해 반복
-        for _ in range(n_cases):
-            # 각 케이스의 레코드 수를 읽어옴 (현재는 사용하지 않지만 기록을 위해 읽음)
-            n_records = int(file.readline().strip())
+        result = [
+            (int(file.readline().strip()), list(map(int, file.readline().strip().split())))
+            for _ in range(n_cases)
+        ]
 
-            # 레코드 줄을 읽고 공백을 기준으로 숫자로 변환하여 리스트에 저장
-            records = [int(x) for x in file.readline().strip().split()]
-            datas.append([n_records, records])
+        return result
 
-    return datas
+def write_file(filename, results):
+    with open(filename, 'w') as file:
+        for case in results:
+            file.write(f"{case[0]}\n")
 
-if __name__=="__main__":
-    data_sets = read_input_file('replacement_input.txt')
-    m = 5
+            for run in case[1]:
+                file.write(" ".join(str(i) for i in run))
+                file.write("\n")
+
+
+def init_buf_freeze(m, case):
+    return case[:m], [0] * m
+
+
+def find_min(buf, freeze, m, state):
+    min_idx = min(
+        (i for i in range(m) if freeze[i] == state),
+        key=lambda i: buf[i]
+    )
+    return buf[min_idx], min_idx
+
+
+def add_run(runs, run):
+    runs.append(run)
+    return [], len(runs)
+
+
+def update_freeze_and_run(buf, freeze, run, m, state):
+    min_k, min_idx = find_min(buf, freeze, m, state)
+    run.append(min_k)
+    freeze[min_idx] = 2
+    return freeze, run
+
+
+def replacement_selection(data_sets, m):
     result = []
 
-    for case in data_sets: # 하나의 테스트 세트: 전체의 테스트 세트
-        # freeze : is freeze, buf : key value
-        freeze = [0] * m
-        # if freeze[].count(2) == 1:
-        #     print("!")
+    for n, case in data_sets:
+        buf, freeze = init_buf_freeze(m, case)
+        runs, run = [], []
+        n_rums = 0
 
-        buf = [0] * m
-        runs = []
-        run = []
-
-        # print(case[1])
-
-        for i in range(m): # 처음 5개의 key를 buf에 넣기
-            buf[i] = case[1][i]
-
-        for i in range(m, case[0]): # 하나의 key :하나의 테스트세트
-            min_k = float('inf')
-            min_idx = 0
-            if freeze.count(1) == m: # 만약 전부 freeze 됐다면, runs에 현재 run추가하고 run 초기화
-                runs.append(run)
-                run = []
+        for i in range(m, n):
+            if freeze.count(1) == m:
                 freeze = [0] * m
+                run, n_rums = add_run(runs, run)
 
-
-            for j in range(m): # buf의 key값중 freeze 안되면서, 최소 값 찾기
-                if freeze[j] == 0 and buf[j] <= min_k:
-                    min_k = buf[j]
-                    min_idx = j
-            # -> buf에서 최솟값의 인덱스 찾음
-
-            run.append(min_k) # run에 최솟값 넣기
-            buf[min_idx] = case[1][i] # 다음 key값을 최솟값이 있었던 자리에 넣기
-            if buf[min_idx] < min_k: # 만약 다음 key값이 최솟값보다 작다면 freeze == 1
+            min_k, min_idx = find_min(buf, freeze, m, 0)
+            run.append(min_k)
+            buf[min_idx] = case[i]
+            if buf[min_idx] < min_k:
                 freeze[min_idx] = 1
 
+        if freeze.count(0) == 0:
+            run, n_rums = add_run(runs, run)
+
         while freeze.count(2) != m:
-            if freeze.count(0) == 0:
-                runs.append(run)
-                run =[]
+            state = 0 if freeze.count(0) else 1
+            freeze, run = update_freeze_and_run(buf, freeze, run, m, state)
+            if freeze.count(state) == 0:
+                run, n_rums = add_run(runs, run)
 
-            if freeze.count(0):
-                min_k = float('inf')
-                min_idx = 0
-                for i in range(m):
-                    if freeze[i] == 0 and buf[i] <= min_k:
-                        min_k = buf[i]
-                        min_idx = i
+        result.append([n_rums, runs])
 
-                run.append(min_k)
-                freeze[min_idx] = 2
-                if freeze.count(0) == 0:
-                    runs.append(run)
-                    run = []
+    return result
 
-            elif freeze.count(1):
-                min_k = float('inf')
-                min_idx = 0
-                for i in range(m):
-                    if freeze[i] == 1 and buf[i] <= min_k:
-                        min_k = buf[i]
-                        min_idx = i
+# python 3.11
+if __name__ == "__main__":
+    m = 5
+    data_sets = read_file('replacement_input.txt')
+    result = replacement_selection(data_sets, m)
+    write_file('replacement_output.txt', result)
 
-                run.append(min_k)
-                freeze[min_idx] = 2
-                if freeze.count(1) == 0:
-                    runs.append(run)
-                    run = []
-        result.append(runs)
+    # 결과 출력
+    print(f"\n총 {len(result)}개")
+    for i, case in enumerate(result, start=1):
+        print(f"\nCase {i}: \n{case[0]}개")
+        for run in case[1]:
+            print(f"Run - {run}")
 
-    for i in range(2):
-        print(result[i])
-
-
-
-
-# # 데이터 출력 (테스트용)
-# for index, case in enumerate(data):
-#     print(f"Test Case {index + 1}: {case}")
