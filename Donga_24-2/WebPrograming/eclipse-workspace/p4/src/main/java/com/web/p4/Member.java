b7f747b538df1d63a2eff200de3ff3015b8b156b
EnthuAI-woolim <markyu428@gmail.com>
2024-11-05 20:47:32 +0900

    2024-11-05 20:47:32

---
+++ Donga_24-2/WebPrograming/eclipse-workspace/p4/src/main/java/com/web/p4/Member.java
@@ -0,0 +1,13 @@
+package com.web.p4;
+
+public class Member {
+	
+	public String mid; /* 필드 2개 */
+	public String pw;
+	
+	public Member(String mid, String pw) { /* 생성자 */
+		this.mid = mid;
+		this.pw = pw;
+	}
+	
+}
--- Donga_24-2/WebPrograming/eclipse-workspace/p4/src/main/java/com/web/p4/ShoppingController.java
+++ Donga_24-2/WebPrograming/eclipse-workspace/p4/src/main/java/com/web/p4/ShoppingController.java
@@ -1,8 +1,11 @@
 package com.web.p4;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import jakarta.servlet.http.HttpSession;
 
@@ -17,5 +20,47 @@ public class ShoppingController {
 		return "login";
 	}
 
+	
+	@GetMapping("/login/check")
+	public String loginCheck(HttpSession se, @RequestParam("mid") String mid, @RequestParam("pw") String pw) {
+		var arr = new ArrayList<Member>(); 
+		arr.add(new Member("고흐","g")); arr.add(new Member("james","j"));
+		arr.add(new Member("dooli","d")); arr.add(new Member("iu","i"));
+		
+		boolean ourMember=false;
+		for(var a: arr) {
+			if(a.mid.equals(mid) && a.pw.equals(pw)) 
+			{ ourMember = true; break; }
+		} 
+		
+		if( ourMember ) { 
+			se.setAttribute("mid", mid); 
+			return "redirect:/menu";  // redirect: -> 'html화면이 아니라, url이 "/menu"인 메소드를 실행시켜라'라는 뜻
+		} 
+		else { 
+			se.setAttribute("msg", mid + "는 미등록 아이디이거나 혹은 패스워드가 일치하지 않습니다.\n"
+					+ "확인 후 로그인 부탁드립니다.");
+			return "redirect:/popup"; 
+		} 
+	}
+	
+	@GetMapping("/menu")
+	public String menu(HttpSession se, Model mo) {
+		mo.addAttribute("mid", se.getAttribute("mid"));
+		return "menu";
+	}
+	
+	@GetMapping("/popup")
+	public String popup(HttpSession se, Model mo) { 
+		mo.addAttribute("msg", se.getAttribute("msg")); 
+		se.removeAttribute("msg");
+		return "popup";
+	}
+	
+	@GetMapping("/coffee")
+	public String coffee(HttpSession se, Model mo) {
+		mo.addAttribute("mid", se.getAttribute("mid"));
+		return "coffee";
+	}
 
 }
